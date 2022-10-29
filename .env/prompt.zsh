c731fb0b0f282ac1d1e4e7018fd78817c175dfbc
michalspano <michal.spano03@gmail.com>
2022-10-29 22:29:17 +0200

    Update `.env/*`; minor reformatting

--- .env/prompt.zsh
+++ .env/prompt.zsh
@@ -6,6 +6,7 @@ zstyle ':vcs_info:git:*' formats '[%b] '
 setopt PROMPT_SUBST
 
 ARROW="»"
-CUSTOM_SYMBOL=" Λ"
+#CUSTOM_SYMBOL=" Λ"
+CUSTOM_SYMBOL=" λ"
 
 PROMPT='%(?.%F{cyan}${CUSTOM_SYMBOL}.%F{red}?%?)%f %B%F{250}%1 %c%f%b %F{cyan}${ARROW}%f %F{cyan}${vcs_info_msg_0_}%f'
--- .env/tmux.zsh
+++ .env/tmux.zsh
@@ -1,4 +1,7 @@
 # ----- { Prompt } ---------------------------------------------------------------------
-if [ ! -z "$TMUX" ]; then
-    echo "\033[0;36m ~ tmuxed ٩( ᐛ )و\n\033[0;m"
-fi
+# everytime tmux is called, this message is printed
+
+EMOJI="٩( ᐛ )و"                     # my custom emoji
+CYAN="\033[0;36m"; RESET="\033[0;m" # colors
+
+if [ ! -z "$TMUX" ]; then echo "${CYAN} ~ tmuxed ${EMOJI}${RESET}\n"; fi
